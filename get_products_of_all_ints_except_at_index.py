5c9755f638851757a8d78c609c188336d171403b
Gabriela Gerson <gabgerson1@gmail.com>
2020-10-21 23:46:26 -0700

    Create get_products_of_all_ints_except_at_index.py

---
+++ get_products_of_all_ints_except_at_index.py
@@ -0,0 +1,32 @@
+def get_products_of_all_ints_except_at_index(arr):
+
+    if len(arr) < 2:
+        return arr
+
+    products_excluding_current = [None] * len(arr)
+    product_so_far = 1
+
+
+    
+
+
+    for i in range(len(arr)):
+        products_excluding_current[i] = product_so_far 
+
+        product_so_far = product_so_far * arr[i]
+
+    # traverse list backwards
+
+
+    product_so_far = 1
+
+    for i in range(len(arr)-1, -1, -1):
+        print(i)
+        products_excluding_current[i] *= product_so_far 
+        product_so_far = product_so_far * arr[i]
+
+    return products_excluding_current
+
+
+
+print(get_products_of_all_ints_except_at_index([1, 0, 3, 4]))
